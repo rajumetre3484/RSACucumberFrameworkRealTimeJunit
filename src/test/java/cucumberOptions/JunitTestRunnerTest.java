7cfc5b949643c829c9224664875a7f74d684d4ed
rajummetre2 <rajummetre2@gmail.com>
2024-01-08 23:30:04 +0530

    my commit

---
+++ src/test/java/cucumberOptions/JunitTestRunnerTest.java
@@ -0,0 +1,19 @@
+package cucumberOptions;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/java/features", glue="stepDefinitions",
+                 monochrome = true,dryRun = false, tags="@PlaceOrder or @OffersPage",
+                 plugin = {"html:target/cucumber.html", "json:target/cucumber.json",
+                		 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                		 "rerun:target/failed_scenarios.txt"})
+
+public class JunitTestRunnerTest 
+{
+
+	
+}
